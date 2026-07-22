f2599752426480cc32b2087fb65be1992245f36e
thesamuelkohl-commits <thesamuelkohl@gmail.com>
2026-07-22 16:25:05 -0500

    Add files via upload

--- generate-reviews.js
+++ generate-reviews.js
@@ -135,6 +135,17 @@ function renderReviewPage(place) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2V4D6ZQV6Q"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-2V4D6ZQV6Q');
+  </script>
+
   <title>${escapeHtml(title)}</title>
   <meta name="description" content="${escapeHtml(description)}" />
   <meta name="robots" content="index, follow" />
